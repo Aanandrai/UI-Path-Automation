6ab8b8543f7f4ce839441ae23633a0ee145a7abd
Aanand Rai <aanand.rai@gigaforce.io>
2025-06-15 12:41:57 +0530

    Basic complete

---
+++ Basic Level2 Loops, ControlFlow/4. Caluclating VAT/.codedworkflows/ConnectionsManager.cs
@@ -0,0 +1,12 @@
+using UiPath.CodedWorkflows;
+using System;
+
+namespace CaluclatingVAT
+{
+    public class ConnectionsManager
+    {
+        public ConnectionsManager(ICodedWorkflowsServiceContainer resolver)
+        {
+        }
+    }
+}
